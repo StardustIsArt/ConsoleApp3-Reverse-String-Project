83c82975edd40ccb2f2e33480c1b29be5a2a9feb
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2025-09-01 17:37:49 -0500

    created Reverse String App

---
+++ ConsoleApp3-Reverse String Project/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp3_Reverse_String_Project;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome. Please input some text: ");
+        string userInput = Console.ReadLine();
+        
+        Console.WriteLine(userInput);
+    }
+}

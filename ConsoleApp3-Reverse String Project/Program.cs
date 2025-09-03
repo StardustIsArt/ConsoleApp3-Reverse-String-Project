db25935fc207370a70dedbed2f55f87e050149e7
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2025-09-03 12:31:13 -0500

    made a for loop for the users input and reverses it

--- ConsoleApp3-Reverse String Project/Program.cs
+++ ConsoleApp3-Reverse String Project/Program.cs
@@ -6,9 +6,25 @@ class Program
     {
         Console.WriteLine("Welcome. Please input some text: ");
         string userInput = Console.ReadLine();
-        char[] charArray = userInput.ToCharArray();
+        string reversedUserInput = "";
+        if (userInput is null)
+        {
+            Console.WriteLine("You have not entered any text. Please try again.");
+        }
+        if (userInput is not null)
+        {
+            for (int i = userInput.Length -1; i >= 0; i--)
+            {
+                reversedUserInput += userInput[i];
+            }
+            Console.WriteLine($"The reversed version of your input is: {reversedUserInput}");
+        }
+        
+        
+        
+        /* char[] charArray = userInput.ToCharArray();
         Array.Reverse(charArray);
         string userInputReversed = new string(charArray);
-        Console.WriteLine(userInputReversed);
+        Console.WriteLine(userInputReversed); */
     }
 }
